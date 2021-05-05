b02b442b6d7d0d9d8c6a536a195d5d9614600ea7
¨1kimberly¨ <kimberly_morales@ispaeducacion.edu.co>
2021-05-05 17:45:52 -0500

    javaScript mayo 5}

---
+++ js/jsicript3.js
@@ -0,0 +1,15 @@
+function asistencia() {
+    let estudiante = document.getElementById("respuesta").value;
+       
+ if (estudiante === "presente"){
+    alert(" el estudiante esta presente")
+ }
+ else{
+    alert( "el estuduante no esta presente");
+}
+   
+/* if (estudiante === false){
+alert(" el estudiante no esta presente ")}*/
+ 
+
+}
---
+++ js/script2.js
@@ -0,0 +1,15 @@
+
+function comprar() {
+    let gansito = false;
+    let chocoBar = false; 
+ // || es ó && es y//
+if (gansito === true) {
+    alert("Toma tu gancito");
+} 
+ if (chocoBar === true && gansito !== true) {
+     alert("toma tu barra de chocorramo");
+ }
+ if (chocoBar === false && gansito === false){
+     alert("No hay ninguno")
+ }
+} 
